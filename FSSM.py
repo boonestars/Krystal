46ba24d9bcb9f8d42346941b003411176496fb77
vagrant <vagrant@experien-86aa6f.(none)>
2015-06-06 13:42:31 -0700

    port kirby fssm to falco

--- FSSM.py
+++ FSSM.py
@@ -4,100 +4,84 @@ import subprocess
 import time
 from win32com.client import GetObject
 import ctypes
-from ConfigParser import NoOptionError, ConfigParser
 
 PROCESS_TERMINATE = 1
 PROC_NAME = "fivestars.exe"
 
-config = ConfigParser()
+import json
 
-class FakeSecHead(object):
-    def __init__(self, fp):
-        self.fp = fp
+with open('c:\\git\\falco-cef\\app\\config.json') as data_file:
+    config = json.load(data_file)
 
-class FakeSecHead(object):
-    def __init__(self, fp):
-        self.fp = fp
-        self.sechead = '[properties_section]\n'
-    def readline(self):
-        if self.sechead:
-            try: return self.sechead
-            finally: self.sechead = None
-        else: return self.fp.readline()
 
 def main():
-	if count_of_processes() > foh_boh_status():
-		terminate_processes()
-	elif count_of_processes() == foh_boh_status()-1:
-		terminate_processes()
-	elif count_of_processes() < foh_boh_status()-1:
-		terminate_processes()
-	while (PROC_NAME in subprocess.Popen('tasklist', stdout=subprocess.PIPE).communicate()[0]) and (count_of_processes() == foh_boh_status()):
-           		time.sleep(10)
-			if (PROC_NAME not in subprocess.Popen('tasklist', stdout=subprocess.PIPE).communicate()[0]) or (count_of_processes() < foh_boh_status()):
-				terminate_processes()
-	terminate_processes()
-	start()
-	time.sleep(loading_time())
-	main()
+    print 'start'
+    if count_of_processes() != foh_boh_status():
+        print 'count of proc != foh boh status'
+        terminate_processes()
+
+    while (PROC_NAME in subprocess.Popen('tasklist', stdout=subprocess.PIPE).communicate()[0]) and (count_of_processes() == foh_boh_status()):
+            time.sleep(10)
+            if (PROC_NAME not in subprocess.Popen('tasklist', stdout=subprocess.PIPE).communicate()[0]):
+                print 'proc no in tasklist'
+                if (count_of_processes() < foh_boh_status()):
+                    print 'count < foh boh status'
+                    terminate_processes()
+    terminate_processes()
+    start()
+    time.sleep(loading_time())
+    main()
 
 def foh_boh_status():
-	try:
-    		config.readfp(FakeSecHead(open('c:\\fivestars\config.ini')))
-	except:
-		sys.exit()
+    # import pdb
+    # pdb.set_trace()
+    print config['plugins']['receipt'].keys()
+    if (config['plugins']['auto_points']['feature']) and (config['plugins']['receipt']['com_monitor']['spmc_installed'] == 'monitoring'):
+        print 'spmc monitoring'
+        auto_points = 1
+    else:
+        print 'spmc not monitoring'
+        auto_points = 0
 
-	try:
-		config.get('plugins', 'auto_points') == "True"
-		try:
-			config.get('plugins', 'serial_port_monitor') == "True"
-			auto_points = 1
-		except NoOptionError:
-			auto_points = 0
-	except:
-		auto_points = 0
-		pass
+    if config['system']['mode'] == "slave":
+        print 'fivestars setup as slave'
+        processes_running = 1 + auto_points
+    elif (config['system']['mode'] == "master") and not (config['system']['serve']):
+        processes_running = 1 + auto_points
+    else:
+        processes_running = 2 + auto_points
 
-	if (config.get('system','mode') == "master"):
-		try:
-			if (config.get('system','serve') == 'False'):
-				processes = 1 + auto_points
-			else:
-				processes = 2 + auto_points
-				return processes
-
-		except NoOptionError:
-			processes = 2 + auto_points
-		return processes
-	elif (config.get('system','mode') == "slave"):
-		processes = 1 + auto_points
-		return processes
+    print "processes_running: ", processes_running
+    return processes_running
 
 def terminate_processes():
-	instance_pid = GetObject('winmgmts:').ExecQuery("Select * from Win32_Process where Name = '%s'" % PROC_NAME)
-	for i in instance_pid:
-		handle = ctypes.windll.kernel32.OpenProcess(PROCESS_TERMINATE, False, i.ProcessID)
-		ctypes.windll.kernel32.TerminateProcess(handle, -1)
-		ctypes.windll.kernel32.CloseHandle(handle)
+    instance_pid = GetObject('winmgmts:').ExecQuery("Select * from Win32_Process where Name = '%s'" % PROC_NAME)
+    for i in instance_pid:
+        handle = ctypes.windll.kernel32.OpenProcess(PROCESS_TERMINATE, False, i.ProcessID)
+        ctypes.windll.kernel32.TerminateProcess(handle, -1)
+        ctypes.windll.kernel32.CloseHandle(handle)
 
 def count_of_processes():
-	running_processes = GetObject('winmgmts:').ExecQuery("Select * from Win32_Process where Name = 'fivestars.exe'").count
-	return running_processes
+    running_processes = GetObject('winmgmts:').ExecQuery("Select * from Win32_Process where Name = 'fivestars.exe'").count
+    print 'running_processes: ', running_processes
+    return running_processes
 
 def fs_exists():
-	PROC_NAME in subprocess.Popen('tasklist', stdout=subprocess.PIPE).communicate()[0]
-	return
+    PROC_NAME in subprocess.Popen('tasklist', stdout=subprocess.PIPE).communicate()[0]
+    return
 
 def start():
-	subprocess.Popen(['c:\\fivestars\\fivestars.exe'])
+    subprocess.Popen(['c:\\fivestars\\fivestars.exe'])
 
 def loading_time():
-	try:
-		timeout = int(config.get('system', 'process_restart_delay'))
-	except NoOptionError:
-		timeout = 20
-	return timeout
+    try:
+        print 'loadig time method'
+        timeout = config['system']['service_manager']['process_restart_delay']
+    except:
+        print 'except loading time method'
+        timeout = 20
+    return timeout
 
 if __name__ == '__main__':
     res = main()
-    sys.exit(res)
+    # sys.exit(res)
